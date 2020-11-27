537b5af95759fe7715cdfaf4781256764c941913
Mikey <michaelp.esteban@gmail.com>
2020-11-27 09:22:16 -0500

    changed footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,6 @@ const Footer = () => {
   const P = document.createElement('P');
   const text = [
     '<strong>Site</strong> by <a href="#">Mikey Esteban</a>.',
-    'The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.',
-    'The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.'
   ]
   P.innerHTML = text.join(' ');
 
