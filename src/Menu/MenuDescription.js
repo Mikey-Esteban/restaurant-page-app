3dcaa9cd1d5d13b57b22db763261c70767a93ce2
Mikey <michaelp.esteban@gmail.com>
2020-11-29 11:04:11 -0500

    fixed position of Menu food images

--- src/Menu/MenuDescription.js
+++ src/Menu/MenuDescription.js
@@ -1,12 +1,13 @@
 const MenuDescription = () => {
 
   const menuINFO = document.createElement('DIV');
-  menuINFO.classList.add('column', 'is-4', 'has-text-centered');
+  menuINFO.classList.add('column', 'has-text-centered');
+  menuINFO.style.maxWidth = '256px';
   const H1 = document.createElement('H1');
-  H1.classList.add('title', 'has-text-link', 'is-size-5');
+  H1.classList.add('title', 'has-text-link', 'has-text-weight-normal', 'is-size-5');
   H1.id = 'food-description';
   const H2 = document.createElement('H2');
-  H2.classList.add('subtitle', 'has-text-link', 'is-size-6');
+  H2.classList.add('subtitle', 'has-text-link', 'has-text-weight-bold', 'is-size-5', 'is-italic');
   H2.id = 'food-price';
   menuINFO.append(H1, H2);
 
--- src/Menu/MenuSection.js
+++ src/Menu/MenuSection.js
@@ -5,7 +5,7 @@ const MenuSection = () => {
   const COLUMNS = document.createElement('DIV');
   COLUMNS.classList.add('columns');
   const menuCOLUMN = document.createElement('DIV');
-  menuCOLUMN.classList.add('column', 'is-one-quarter', 'is-offset-1', 'menu-food-column');
+  menuCOLUMN.classList.add('column', 'is-one-quarter', 'is-offset-2', 'menu-food-column');
   const imgCOLUMN = document.createElement('DIV');
   imgCOLUMN.classList.add('column', 'auto', 'img-food-column');
 
